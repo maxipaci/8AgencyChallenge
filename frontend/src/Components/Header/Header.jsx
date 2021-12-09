d48d8736a9c1a63b48745c7c33534c4c21ae72c9
Maximiliano Paci <pacimaximiliano@gmail.com>
2021-12-08 22:56:22 -0300

    adding header component

---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+
+  return (
+    <div className="Home" >
+      <img src="/images/assets/HeaderBackground.svg" alt="Wave"/>
+      <div className="Webinar" >WEBINAR</div>
+      <div className="TextArea">
+        <h1>El reto de humanizar el CX financiero en 2021.</h1>
+        <h1>La experiencia de un unicornio de Latam</h1>
+        <p>MIÉRCOLES 16 DE DICIEMBRE 17 HS (HORARIO DE QUITO)</p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
